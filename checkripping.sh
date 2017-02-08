995ae02f3c4560a57823c00c10f9ead1b65e8913
Scuttle <scuttle@gmail.com>
2017-02-08 12:03:35 +0100

    updated cronscript

--- checkripping.sh
+++ checkripping.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 RIPPID=`ps -ef|grep "rip_authors.pl"|grep -v grep|awk '{print $2}'`
-AUTHORFILE=`find /var/www/authors/ -mmin +1 -type f`
+JAVAPID=`ps -ef|grep java|grep -v "sh -c"|grep -v grep|awk '{print $2}'`
+AUTHORFILE=`find /var/www/authors/ -mmin +15 -type f`
 
 if [ -z "$RIPPID" ]
 then
-  echo "No rip running, starting rip"
+#  echo "No rip running, starting rip"
   cd /var/www
   nohup perl rip_authors.pl &
   exit
@@ -13,11 +14,14 @@ fi
 
 if [ -n "$AUTHORFILE" ]
 then
-  echo "Authorfile is old, restarting rip"
+#  echo "Authorfile is old, restarting rip"
   kill -9 $RIPPID
+  sleep 2
+  kill -9 $JAVAPID
+  sleep 2
   cd /var/www
   nohup perl rip_authors.pl &
   exit
 fi
 
-echo "Rip is running, authorfile is new enough, everything OK"
+#echo "Rip is running, authorfile is new enough, everything OK"
